896d2d2dd12efb627849d9ec0e55079cf704af05
kwihyeon <imleo322@gmail.com>
2025-09-24 17:08:34 +0900

    first commit

---
+++ src/main/java/com/fastcampus/exercisereservationsystem/ExerciseReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.exercisereservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/exercisereservationsystem/ExerciseReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.exercisereservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
