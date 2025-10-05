3a3f78efec8ac32963a5a3943586109b44a0f3c8
kwihyeon <imleo322@gmail.com>
2025-10-06 00:38:57 +0900

    feat : 리드미 인틀리제이에서 삭제

--- src/main/frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["../resources/templates/**/*.{html,js,mustache}"], // mustache는 사용하지 않을 경우 삭제
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
