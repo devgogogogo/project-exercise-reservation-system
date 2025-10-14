4d51b4292dfafbe074b04981a1214e6d45ef8c19
kwihyeon <imleo322@gmail.com>
2025-10-14 09:27:55 +0900

    feat : CI 오류로 인한 수정 5차

--- src/test/java/com/fastcampus/exercisereservationsystem/ExerciseReservationSystemApplicationTests.java
+++ src/test/java/com/fastcampus/exercisereservationsystem/ExerciseReservationSystemApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.MySQLContainer;
 
 @SpringBootTest
 @ActiveProfiles("test")
--- src/test/java/com/fastcampus/exercisereservationsystem/domain/ContainersConfig.java
+++ src/test/java/com/fastcampus/exercisereservationsystem/domain/ContainersConfig.java
@@ -1,19 +1,32 @@
 package com.fastcampus.exercisereservationsystem.domain;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @TestConfiguration(proxyBeanMethods = false)
+@Testcontainers
 public class ContainersConfig {
 
 
-    @ServiceConnection
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0");
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.36");
 
-    @ServiceConnection // spring.data.redis.* 자동 주입
-    static GenericContainer<?> redis = new GenericContainer<>("redis:7.2").withExposedPorts(6379);
+    @Container
+    static GenericContainer<?> redis =
+            new GenericContainer<>("redis:7.2").withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void registerProps(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", mysql::getJdbcUrl);
+        r.add("spring.datasource.username", mysql::getUsername);
+        r.add("spring.datasource.password", mysql::getPassword);
+
+        r.add("spring.data.redis.host", redis::getHost);
+        r.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+    }
 }
