1a4c0851953967d32f81f1f544666085382cc4cb
kwihyeon <imleo322@gmail.com>
2025-10-28 11:30:09 +0900

    feat : 공지사항에 댓글 생성, 수정 삭제 기능 추가 추후에 대댓글  추가해볼예정

--- src/main/java/com/fastcampus/exercisereservationsystem/domain/comment/dto/response/CreateCommentResponse.java
+++ src/main/java/com/fastcampus/exercisereservationsystem/domain/comment/dto/response/CreateCommentResponse.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 public record CreateCommentResponse(
         Long commentId,
-        String username,
+        String nickname,
         String description,
         LocalDate modifiedAt
 ) {
@@ -14,7 +14,7 @@ public record CreateCommentResponse(
     public static CreateCommentResponse from(CommentEntity entity) {
         return new CreateCommentResponse(
                 entity.getId(),
-                entity.getUser().getUsername(),
+                entity.getUser().getNickname(),
                 entity.getDescription(),
                 entity.getModifiedAt().toLocalDate()
         );
